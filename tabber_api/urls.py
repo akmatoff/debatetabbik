698288dcc728ed839bf007e5595e16d20ba70a24
akmatoff <akmatoff.exe@gmail.com>
2024-03-20 21:49:34 +0600

    Add static urls

--- tabber_api/urls.py
+++ tabber_api/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
     path('api/', include('users.urls')),
     path('api/', include('clubs.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+ 
