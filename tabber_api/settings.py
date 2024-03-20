9108fd938f673f396820645be8906df4396c4321
akmatoff <akmatoff.exe@gmail.com>
2024-03-20 19:21:59 +0600

    Add static root

--- tabber_api/settings.py
+++ tabber_api/settings.py
@@ -19,7 +19,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_ROOT = "static/"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
