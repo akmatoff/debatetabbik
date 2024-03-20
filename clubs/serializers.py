446152c95c9d9fc9b403813495a1d2d151934b9a
akmatoff <akmatoff.exe@gmail.com>
2024-03-21 03:30:17 +0600

    Fix club leader include in club list and details

--- clubs/serializers.py
+++ clubs/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from .models import Club
 from users.serializers import UserSerializer
-class ClubSerializer(serializers.ModelSerializer):
-    # club_leader = UserSerializer(read_only=True)
 
+class ClubSerializer(serializers.ModelSerializer):
+    club_leader = UserSerializer(read_only=True)
     class Meta:
         model = Club
         fields = ['id', 'title', 'club_leader']
--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'club_id', 'email']
+        fields = ['id', 'username', 'first_name', 'last_name', 'club', 'email']
+
+    def to_representation(self, instance):
+        return {
+            "id": instance.id,
+            "username": instance.username,
+            "email": instance.email,
+            "club": instance.club_id
+        }
