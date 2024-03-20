117fb054a94798f17a1b58d5a95c23f8d013a26c
akmatoff <akmatoff.exe@gmail.com>
2024-03-21 04:12:42 +0600

    Fix club serializer

--- clubs/serializers.py
+++ clubs/serializers.py
@@ -4,6 +4,8 @@ from users.serializers import UserSerializer
 
 class ClubSerializer(serializers.ModelSerializer):
     club_leader = UserSerializer(read_only=True)
+    club_leader_id = serializers.IntegerField(write_only=True)
+
     class Meta:
         model = Club
-        fields = ['id', 'title', 'description', 'icon', 'club_leader']
+        fields = ['id', 'title', 'description', 'icon', 'club_leader', 'club_leader_id']
