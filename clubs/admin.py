7a417db9960119dcd2e313ac778be8f41fb392a8
akmatoff <akmatoff.exe@gmail.com>
2024-03-21 06:04:24 +0600

    Club join request admin

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Club
+from .models import Club, ClubJoinRequest
 
 # Register your models here.
 class ClubsAdmin(admin.ModelAdmin):
     list_display = ('title', 'icon', 'rating')
 
+class ClubJoinRequestAdmin(admin.ModelAdmin):
+    list_display = ('user', 'club', 'is_approved')
+
 admin.site.register(Club, ClubsAdmin)
+admin.site.register(ClubJoinRequest, ClubJoinRequestAdmin)
