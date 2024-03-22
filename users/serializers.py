3bd1934ec76f8981fd14d99e49174093ec3ee6bf
akmatoff <akmatoff.exe@gmail.com>
2024-03-22 23:14:37 +0600

    Fix club None handling

--- users/serializers.py
+++ users/serializers.py
@@ -1,23 +1,23 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'club', 'email']
+        fields = ["id", "username", "first_name", "last_name", "club_id", "email"]
 
     def to_representation(self, instance):
+        club = None
+
+        if instance.club_id:
+            club = instance.club_id
+
         return {
             "id": instance.id,
             "username": instance.username,
             "first_name": instance.first_name,
             "last_name": instance.last_name,
             "email": instance.email,
-            "club": {
-                "id": instance.club.id,
-                "title": instance.club.title,
-                "description": instance.club.description,
-                "rating": instance.club.rating,
-                "icon": str(instance.club.icon),
-                "club_leader_id": instance.club.club_leader_id
-            }
+            "club_id": club,
         }
--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from rest_framework import generics, permissions
 from rest_framework.views import APIView, Response
 from .models import User
 from .serializers import UserSerializer
+from clubs.models import Club
+from clubs.serializers import ClubSerializer
+
 
 class UsersList(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -9,11 +12,13 @@ class UsersList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class UserDetails(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class UserData(APIView):
     def get(self, request):
@@ -21,4 +26,9 @@ class UserData(APIView):
 
         serializer = UserSerializer(user)
 
-        return Response(serializer.data)
+        if user.club:
+            club = Club.objects.get(pk=user.club.id)
+
+            club_serializer = ClubSerializer(club)
+
+        return Response({"user": serializer.data, "club": club_serializer.data})
