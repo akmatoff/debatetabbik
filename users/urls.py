33a2b23c92fbb800d7c078384af9a4e7d50032da
akmatoff <akmatoff.exe@gmail.com>
2024-04-02 16:36:18 +0600

    Include google auth callback url

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserDetails, UsersList, UserData
+from .views import UserDetails, UsersList, UserData, google_auth_callback
 
 urlpatterns = [
     path("users/", UsersList.as_view(), name="Users list"),
     path("users/<int:pk>", UserDetails.as_view(), name="User details"),
     path("get_userdata/", UserData.as_view(), name="User data"),
+    path("google-auth-callback/", google_auth_callback, name="Google auth callback"),
 ]
