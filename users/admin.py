13bf656d021e37d0c66a6f28806021a8c1395c7d
akmatoff <akmatoff.exe@gmail.com>
2024-04-02 16:35:13 +0600

    Include avatar in admin

--- users/admin.py
+++ users/admin.py
@@ -3,24 +3,37 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
+
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'is_staff', 'first_name', 'last_name', 'club')
-    fieldsets = (
-        (None, {
-            'fields': ('username', 'password')
-        }),
-        ('Personal info', {
-            'fields': ('first_name', 'last_name', 'email', 'club')
-        }),
-        ('Permissions', {
-            'fields': (
-                'is_active', 'is_staff', 'is_superuser',
-                'groups', 'user_permissions'
-                )
-        }),
-        ('Important dates', {
-            'fields': ('last_login', 'date_joined')
-        }),
+    list_display = (
+        "username",
+        "email",
+        "avatar",
+        "is_staff",
+        "first_name",
+        "last_name",
+        "club",
     )
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {"fields": ("avatar", "first_name", "last_name", "email", "club")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
 
 admin.site.register(User, CustomUserAdmin)
